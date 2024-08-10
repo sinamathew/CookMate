a4def6402cc67aa7e69f6815bc9a7fad90f683be
Sina Mathew <mathewoluwashinastephen@gmail.com>
2024-08-11 00:34:58 +0100

    Exhusted

--- recipe.js
+++ recipe.js
@@ -1,18 +1,27 @@
 class Recipe {
-  constructor(name, description, ingredents, instructions, dietInfo) {
+  #ingredents;
+  #instructions;
+  #dietInfo;
+
+  constructor(name, description, ingredents, instructions, dietInfo, additionalInfo) {
     this.name = name;
     this.description = description;
-    this.ingredents = ingredents;
-    this.instructions = instructions;
-    this.dietary_info = dietInfo;
+    this.#ingredents = ingredents;
+    this.#instructions = instructions;
+    this.#dietInfo = dietInfo;
+    this.additionalInfo = additionalInfo;
   }
 }
 
 
-const createRecipe = (name, description, ingredents, instructions, dietInfo) => {
-  return new Recipe(name, description, ingredents, instructions, dietInfo);
+const createRecipe = (name, description, ingredents, instructions, dietInfo, additionalInfo) => {
+  return new Recipe(name, description, ingredents, instructions, dietInfo, additionalInfo);
 }
 
-const nigerianJollof = createRecipe("Nigerian Jollof", "A delicious meal made with rice in Nigerian style.", ['rice', 'onga', 'salt', 'cray fish', 'green bean', 'carrot'], "I dont know", "I dont know");
+const nigerianJollof = createRecipe("Nigerian Jollof", 
+  "A delicious meal made with rice in Nigerian style.", 
+  ['rice', 'vegetable oil', 'beef', 'ponmo', 'ogufe', 'salt', 'crayfish', 'green bean', 'carrot', 'onions', 'catfish', 'tomatoes', 'cammeroun pepper' ],
+  ['get all neccesary ingredents', 'wash and preboil your rice for 800 years', 'then wait'],
+  "I dont know yet", { cookTime: 'Eternity', serves: 4 });
 
 console.log(nigerianJollof);
