fc1934b7a33149e1da63e4dd7a352f81cdc36eb6
mathewoluwashinastephen <mathewoluwashinastephen@gmail.com>
2024-08-11 08:18:39 +0000

    Project IDX setup

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from 'express';
+const app = express();
+
+app.get('/', (req, res) => {
+  const name = process.env.NAME || 'World';
+  res.send(`Hello ${name}!`);
+});
+
+const port = parseInt(process.env.PORT) || 3000;
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
